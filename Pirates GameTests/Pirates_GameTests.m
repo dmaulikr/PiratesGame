e474086205ab2a8f643efe5515770986208991ff
sailnathona <Codeseeds@iMac.local>
2015-08-17 11:25:54 +0200

    Initial Commit

---
+++ Pirates GameTests/Pirates_GameTests.m
@@ -0,0 +1,40 @@
+//
+//  Pirates_GameTests.m
+//  Pirates GameTests
+//
+//  Created by Coceseeds on 17/08/15.
+//  Copyright (c) 2015 Coceseeds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface Pirates_GameTests : XCTestCase
+
+@end
+
+@implementation Pirates_GameTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
